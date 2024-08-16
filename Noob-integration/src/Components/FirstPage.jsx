137166a7ebe98f6c6dc10f12516674b135f36af4
Tiavina3827 <hei.tiavina.4@gmail.com>
2024-08-16 23:35:05 +0300

    feat: responsive of the first page

--- Noob-integration/src/Components/FirstPage.jsx
+++ Noob-integration/src/Components/FirstPage.jsx
@@ -1,26 +1,33 @@
+import React, { useState } from 'react';
 import './Component_stylesheet/FirstPage.css';
+
 function FirstPage() {
+    const [isNavVisible, setIsNavVisible] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavVisible(!isNavVisible);
+    };
 
     return (
         <>
-
-        <header>
-            <nav>
-                <div className="logo"><img src="src/assets/Pictogram.svg" alt="logo"/></div>
-                <div className="navLinks">
-                    <a href="#" className="links" > Park list </a>
-                    <a href="#" className="links" > Evenments </a>
-                    <a href="#" className="links" > AI </a>
-                    <a href="#" className="links" > Biodiversity Stats </a>
+            <header>
+                <nav>
+                    <div className="logo"><img src="src/assets/Pictogram.svg" alt="logo"/></div>
+                    <i className="fa-solid fa-bars" id="menu" onClick={toggleNav}></i>
+                    <div className={`navLinks ${isNavVisible ? 'show' : ''}`}>
+                        <a href="#" className="links"> Park list </a>
+                        <a href="#" className="links"> Events </a>
+                        <a href="#" className="links"> AI </a>
+                        <a href="#" className="links"> Biodiversity Stats </a>
+                    </div>
+                </nav>
+                <div className="mainText">
+                    <h1 className="HeadText"> Road To The </h1>
+                    <h1 className="HeadText1"> Green Island </h1>
                 </div>
-            </nav>
-            <div className="mainText">
-                <h1 className="HeadText"> Road To The  </h1>
-                <h1 className="HeadText1"> Green Island </h1>
-            </div>
-        </header>
+            </header>
         </>
-    )
+    );
 }
 
 export default FirstPage;
